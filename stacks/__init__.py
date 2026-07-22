306a9615b8be208a6f8f2f9776c22af9950f4ecb
Leander Craig <leander@stackgen.com>
2026-07-22 07:17:09 +0000

    feat: add fe-demo-pass CDK stack (SecureBucket, CMK, tagged)

--- stacks/__init__.py
+++ stacks/__init__.py
@@ -4,6 +4,7 @@ from .storage_stack import StorageStack
 from .messaging_stack import MessagingStack
 from .compute_api_stack import ComputeApiStack
 from .database_stack import DatabaseStack
+from .fe_demo_pass_stack import FeDemoPassStack
 
 __all__ = [
     "NetworkStack",
@@ -11,4 +12,5 @@ __all__ = [
     "MessagingStack",
     "ComputeApiStack",
     "DatabaseStack",
+    "FeDemoPassStack",
 ]
